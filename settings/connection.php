b148e7629cbbab935c64c1a77e395d57f4cab160
nicoleineza <nicole.ineza@ashesi.edu.gh>
2024-03-22 17:14:20 +0000

    Create connection.php

---
+++ settings/connection.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_HOST', 'g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'rw5k3fwiketxzlnq');
+define('DB_PASSWORD', 'q9rrky2sg0wazoe7'); 
+define('DB_NAME', 'lm6hn94qfjqgz8j0');
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
+}
+
+?>
