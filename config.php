b20ceeacecac686c52ff54230ca8d9d479f4fd12
nicoleineza <nicole.ineza@ashesi.edu.gh>
2024-03-22 21:40:07 +0000

    kk

--- config.php
+++ config.php
@@ -9,8 +9,6 @@ $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully!";
-}
+} 
 
 ?>
