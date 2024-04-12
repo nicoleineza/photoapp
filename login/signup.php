b4a562d00c66155bec2200ceba9de2bbd67f5433
nicoleineza <nicole.ineza@ashesi.edu.gh>
2024-04-12 23:50:24 +0000

    Update signup.php

--- login/signup.php
+++ login/signup.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Signup Page</title>
-<link rel="stylesheet" href="../css/signup.css">
-<link rel="icon" href="../assets/appicon.png">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="../js/signup.js"></script>
-<style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Page</title>
+    <link rel="stylesheet" href="../css/signup.css">
+    <link rel="icon" href="../assets/appicon.png">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="../js/signup.js"></script>
+    <style>
         .app-icon {
-            width: 50px; 
-            height: auto; 
+            width: 50px;
+            height: auto;
             position: absolute;
-            top: 20px; 
-            left: 20px; 
+            top: 20px;
+            left: 20px;
         }
-</style>
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
 <div class="header">
-        <img src="../assets/appicon.png" alt="App Icon" class="app-icon">
-        <h2>PhotoApp</h2>
-    </div>
+    <img src="../assets/appicon.png" alt="App Icon" class="app-icon">
+    <h2>PhotoApp</h2>
+</div>
 <div class="signup-container">
     <h2>Register to get Started!</h2>
     <form id="signupForm" method="post">
@@ -39,6 +42,7 @@
         <div class="form-group">
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
+            <span id="password_error" class="error"></span>
         </div>
         <div class="form-group">
             <label for="confirm_password">Confirm Password:</label>
@@ -53,9 +57,43 @@
             <label for="non_photographer">No</label>
         </div>
         <button type="submit">Signup</button>
-        <div id="signup_message"></div> 
-        <p>><a href="login.php">have an account? click here to login</a></p>
+        <div id="signup_message"></div>
+        <p><a href="login.php">Have an account? Click here to login</a></p>
     </form>
 </div>
+<script>
+    $(document).ready(function () {
+        $('#signupForm').submit(function (event) {
+            // Validate email
+            var email = $('#email').val();
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(email)) {
+                $('#email_error').text('Please enter a valid email address.');
+                event.preventDefault();
+            } else {
+                $('#email_error').text('');
+            }
+
+            // Validate password
+            var password = $('#password').val();
+            var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+            if (!passwordRegex.test(password)) {
+                $('#password_error').text('Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character.');
+                event.preventDefault();
+            } else {
+                $('#password_error').text('');
+            }
+
+            // Confirm password
+            var confirmPassword = $('#confirm_password').val();
+            if (password !== confirmPassword) {
+                $('#confirm_password_error').text('Passwords do not match.');
+                event.preventDefault();
+            } else {
+                $('#confirm_password_error').text('');
+            }
+        });
+    });
+</script>
 </body>
 </html>
